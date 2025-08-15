eb408520d86a6ddbe235095fa6962634553b686f
Buck <k.buckleys@outlook.com>
2025-08-15 17:14:52 +0300

    Add files via upload

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,31 +1,63 @@
-require("config.lazy") -- your plugin and LazyVim bootstrap
+require("config.lazy")
 
--- Define transparency function
-local function set_transparency()
-  vim.cmd([[
-    hi Normal guibg=NONE ctermbg=NONE
-    hi NormalNC guibg=NONE ctermbg=NONE
-    hi SignColumn guibg=NONE ctermbg=NONE
-    hi StatusLine guibg=NONE ctermbg=NONE
-    hi StatusLineNC guibg=NONE ctermbg=NONE
-    hi VertSplit guibg=NONE ctermbg=NONE
-    hi TabLine guibg=NONE ctermbg=NONE
-    hi TabLineFill guibg=NONE ctermbg=NONE
-    hi TabLineSel guibg=NONE ctermbg=NONE
-    hi Pmenu guibg=NONE ctermbg=NONE
-    hi PmenuSel guibg=NONE ctermbg=NONE
-    hi NeoTreeNormal guibg=NONE ctermbg=NONE
-    hi NeoTreeNormalNC guibg=NONE ctermbg=NONE
-    hi NeoTreeWinSeparator guibg=NONE ctermbg=NONE
-    hi NeoTreeEndOfBuffer guibg=NONE ctermbg=NONE
-    hi EndOfBuffer guibg=NONE ctermbg=NONE
-  ]])
-end
-
--- Now apply transparency *after* colorscheme is active
-set_transparency()
-
-vim.api.nvim_create_autocmd("BufEnter", {
-  pattern = "*",
-  callback = set_transparency,
+require("catppuccin").setup({
+  float = {
+    transparent = true,
+    solid = true,
+  },
+  flavour = "mocha",
+  background = {
+    light = "latte",
+    dark = "mocha",
+  },
+  transparent_background = true,
+  show_end_of_buffer = false,
+  term_colors = true,
+  dim_inactive = {
+    enabled = false,
+    shade = "dark",
+    percentage = 0.15,
+  },
+  no_italic = false,
+  no_bold = false,
+  no_underline = false,
+  styles = {
+    comments = { "italic" },
+    conditionals = { "italic" },
+    loops = {},
+    functions = {},
+    keywords = {},
+    strings = {},
+    variables = {},
+    numbers = {},
+    booleans = {},
+    properties = {},
+    types = {},
+    operators = {},
+  },
+  color_overrides = {},
+  custom_highlights = {},
+  default_integrations = true,
+  integrations = {
+    cmp = true,
+    gitsigns = true,
+    nvimtree = true,
+    treesitter = true,
+    notify = false,
+    mini = {
+      enabled = true,
+      indentscope_color = "",
+    },
+  },
 })
+
+vim.cmd.colorscheme("catppuccin")
+
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+vim.cmd.colorscheme("catppuccin")
