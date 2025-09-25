0862926ba9fa27f39edd72f183dd12859d7bccd8
Buck <kbuck@tuta.io>
2025-09-25 15:47:59 +0300

    Add files via upload

---
+++ .config/yazi.lua
@@ -0,0 +1,58 @@
+--  ▄███████▄     ▄████████ ███▄▄▄▄
+-- ██▀     ▄██   ███    ███ ███▀▀▀██▄
+--       ▄███▀   ███    █▀  ███   ███
+--  ▀█▀▄███▀▄▄  ▄███▄▄▄     ███   ███
+--   ▄███▀   ▀ ▀▀███▀▀▀     ███   ███
+-- ▄███▀         ███    █▄  ███   ███
+-- ███▄     ▄█   ███    ███ ███   ███
+--  ▀████████▀   ██████████  ▀█   █▀
+--
+--  ▄█     █▄   ▄██████▄     ▄████████    ▄█   ▄█▄    ▄████████
+-- ███     ███ ███    ███   ███    ███   ███ ▄███▀   ███    ███
+-- ███     ███ ███    ███   ███    ███   ███▐██▀     ███    █▀
+-- ███     ███ ███    ███  ▄███▄▄▄▄██▀  ▄█████▀      ███
+-- ███     ███ ███    ███ ▀▀███▀▀▀▀▀   ▀▀█████▄    ▀███████████
+-- ███     ███ ███    ███ ▀███████████   ███▐██▄            ███
+-- ███ ▄█▄ ███ ███    ███   ███    ███   ███ ▀███▄    ▄█    ███
+--  ▀███▀███▀   ▀██████▀    ███    ███   ███   ▀█▀  ▄████████▀
+--                          ███    ███   ▀
+--
+--              https://github.com/kbuckleys/
+
+---@type "LazySpec"
+return {
+  "mikavilpas/yazi.nvim",
+  version = "*",
+  event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+  },
+  keys = {
+    {
+      "<leader>-",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      "<leader> ",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open Yazi",
+    },
+    {
+      "<c-up>",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+  ---@type YaziConfig | {}
+  opts = {
+    open_for_directories = false,
+    keymaps = {
+      show_help = "<f1>",
+    },
+  },
+  init = function()
+    vim.g.loaded_netrwPlugin = 1
+  end,
+}
