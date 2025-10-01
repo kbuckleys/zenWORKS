3022aff8e00654f5c6fbad0540cdabd90f75a6a2
Buck <kbuck@tuta.io>
2025-10-01 06:56:11 +0300

    Add files via upload

--- .config/waybar/scripts/gpu-temp.sh
+++ .config/waybar/scripts/gpu-temp.sh
@@ -5,4 +5,8 @@
 
 #!/bin/bash
 GPU_TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits)
-echo "GT $GPU_TEMP°C"
+if [ "$GPU_TEMP" -ge 80 ]; then
+  echo "GT ${GPU_TEMP}°C"
+else
+  echo "GT ${GPU_TEMP}°C"
+fi
