2b759753c36a7d8063e801e689a5dafef0b5377d
Buck <kbuck@tuta.io>
2025-10-23 05:14:55 +0300

    Add files via upload

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,11 @@
+-- ┌─┐┌─┐┌┐┌┬ ┬┌─┐┬─┐┬┌─┌─┐
+-- ┌─┘├┤ │││││││ │├┬┘├┴┐└─┐
+-- └─┘└─┘┘└┘└┴┘└─┘┴└─┴ ┴└─┘
+-- https://github.com/kbuckleys/
+
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.opt.title = true
+vim.opt.titlestring = "%t ~ nvim"
