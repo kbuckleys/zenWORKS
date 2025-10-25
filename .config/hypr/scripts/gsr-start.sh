13b24588ab5d74300ad6e54d29437f1161772156
Buck <kbuck@tuta.io>
2025-10-25 06:49:29 +0300

    Delete .config/hypr/scripts directory

--- .config/hypr/scripts/gsr-start.sh
+++
@@ -1,7 +0,0 @@
-# ┌─┐┌─┐┌┐┌┬ ┬┌─┐┬─┐┬┌─┌─┐
-# ┌─┘├┤ │││││││ │├┬┘├┴┐└─┐
-# └─┘└─┘┘└┘└┴┘└─┘┴└─┴ ┴└─┘
-# https://github.com/kbuckleys/
-
-notify-send "Recording Started" "GPU Screen Recorder"
-gpu-screen-recorder -w screen -s 1920x1080 -f 60 -c mp4 -o ~/Videos/Captures/capture_$(date +%Y-%m-%d_%H-%M-%S).mp4
