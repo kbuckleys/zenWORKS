0e9fc580cab4af2cdfd300872741af22d5d76947
Buck <kbuck@tuta.io>
2025-10-30 10:17:11 +0300

    Add files via upload

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,17 @@
+-- ┌─┐┌─┐┌┐┌┬ ┬┌─┐┬─┐┬┌─┌─┐
+-- ┌─┘├┤ │││││││ │├┬┘├┴┐└─┐
+-- └─┘└─┘┘└┘└┴┘└─┘┴└─┴ ┴└─┘
+-- https://github.com/kbuckleys/
+
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.options.theme = "auto"
+      opts.options.component_separators = " | "
+      opts.options.section_separators = ""
+      opts.options.icons_enabled = true
+    end,
+  },
+}
