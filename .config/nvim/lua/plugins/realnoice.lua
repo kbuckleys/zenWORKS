b30237bd48078ea19018a0d60e44b1d42e076f20
Buck <kbuck@tuta.io>
2025-10-30 11:40:58 +0300

    Add files via upload

---
+++ .config/nvim/lua/plugins/realnoice.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      -- Use native cmdline UI but no popup floating with minimal float styling
+      opts.cmdline = {
+        view = "cmdline", -- use the regular commandline window, not popup
+        format = {}, -- default format
+      }
+      opts.routes = {
+        -- example route to disable message that pushes statusline
+        {
+          filter = { event = "msg_showmode" },
+          opts = { skip = true },
+        },
+      }
+      opts.views = {
+        cmdline_popup = {
+          -- Remove border and background from popup to simulate minimal overlay
+          border = nil,
+          win_options = {
+            winblend = 100, -- fully transparent
+            winhighlight = "NormalFloat:Normal",
+          },
+        },
+      }
+      -- Position the cmdline to overlay at bottom row (statusline position)
+      opts.position = {
+        cmdline = { row = "99%", col = "50%" },
+      }
+      return opts
+    end,
+  },
+}
