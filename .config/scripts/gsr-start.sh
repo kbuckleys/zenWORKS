b559264ef2a11e62f89803022f11d2458a5f8d98
Buck <kbuck@tuta.io>
2025-11-02 11:33:51 +0200

    Add files via upload

--- .config/scripts/gsr-start.sh
+++ .config/scripts/gsr-start.sh
@@ -4,4 +4,4 @@
 # https://github.com/kbuckleys/
 
 notify-send "Recording Started" "GPU Screen Recorder"
-gpu-screen-recorder -w screen -s 1920x1080 -f 60 -c mp4 -o ~/Videos/Captures/capture_$(date +%Y-%m-%d_%H-%M-%S).mp4
+gpu-screen-recorder -w screen -f 60 -a "$(pactl get-default-sink).monitor" -a "$(pactl get-default-source)" -c mp4 -o ~/Videos/Captures/capture_$(date +%Y-%m-%d_%H-%M-%S).mp4
