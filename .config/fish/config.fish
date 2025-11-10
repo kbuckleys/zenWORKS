e6668711f00ec6f4bfdd1d9b7dc36e8755fdcbb6
Buck <kbuck@tuta.io>
2025-11-10 14:15:26 +0200

    Add files via upload

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+# ┌─┐┌─┐┌┐┌┬ ┬┌─┐┬─┐┬┌─┌─┐
+# ┌─┘├┤ │││││││ │├┬┘├┴┐└─┐
+# └─┘└─┘┘└┘└┴┘└─┘┴└─┴ ┴└─┘
+# https://github.com/kbuckleys/
+
+set -g -x fish_greeting ''
+
+function fish_prompt
+    set_color $fish_color_cwd
+    printf '%s ' (prompt_pwd)
+end
+
+# Aliases
+alias ls="eza -G --icons"
