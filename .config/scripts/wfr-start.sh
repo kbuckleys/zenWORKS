5a02a11cc2c6a443b242ed7a18a9ed9492b99376
Buck <kbuck@tuta.io>
2025-11-22 02:43:39 +0000

    Add files via upload

--- .config/scripts/wfr-start.sh
+++ .config/scripts/wfr-start.sh
@@ -3,5 +3,11 @@
 # └─┘└─┘┘└┘└┴┘└─┘┴└─┴ ┴└─┘
 # https://github.com/kbuckleys/
 
+output_dir="$HOME/Videos/Captures"
+
+if [ ! -d "$output_dir" ]; then
+  mkdir -p "$output_dir"
+fi
+
 notify-send "Recording Started" "wf-recorder"
-wf-recorder --file="$HOME/Videos/Captures/capture_$(date +'%Y-%m-%d_%H-%M-%S').mp4"
+wf-recorder --file="$output_dir/recording_$(date +'%Y-%m-%d_%H-%M-%S').mp4"
