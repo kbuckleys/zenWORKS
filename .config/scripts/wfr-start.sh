0c2e20039a81f0217f561dfdafe5c24cb3fece13
Buck <kbuck@tuta.io>
2025-11-22 02:40:16 +0000

    Add files via upload

---
+++ .config/scripts/wfr-start.sh
@@ -0,0 +1,7 @@
+# ┌─┐┌─┐┌┐┌┬ ┬┌─┐┬─┐┬┌─┌─┐
+# ┌─┘├┤ │││││││ │├┬┘├┴┐└─┐
+# └─┘└─┘┘└┘└┴┘└─┘┴└─┴ ┴└─┘
+# https://github.com/kbuckleys/
+
+notify-send "Recording Started" "wf-recorder"
+wf-recorder --file="$HOME/Videos/Captures/capture_$(date +'%Y-%m-%d_%H-%M-%S').mp4"
---
+++ .config/scripts/wfr-stop.sh
@@ -0,0 +1,7 @@
+# ┌─┐┌─┐┌┐┌┬ ┬┌─┐┬─┐┬┌─┌─┐
+# ┌─┘├┤ │││││││ │├┬┘├┴┐└─┐
+# └─┘└─┘┘└┘└┴┘└─┘┴└─┴ ┴└─┘
+# https://github.com/kbuckleys/
+
+notify-send "Recording Stopped" "wf-recorder"
+pkill -SIGINT -f wf-recorder
