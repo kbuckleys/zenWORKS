e54a2e053ee2ccf984a915b92c276207d3c6c1d1
Buck <kbuck@tuta.io>
2025-11-22 02:39:41 +0000

    Delete .config/scripts/gsr-start.sh

--- .config/scripts/gsr-start.sh
+++
@@ -1,7 +0,0 @@
-# ┌─┐┌─┐┌┐┌┬ ┬┌─┐┬─┐┬┌─┌─┐
-# ┌─┘├┤ │││││││ │├┬┘├┴┐└─┐
-# └─┘└─┘┘└┘└┴┘└─┘┴└─┴ ┴└─┘
-# https://github.com/kbuckleys/
-
-notify-send "Recording Started" "GPU Screen Recorder"
-gpu-screen-recorder -w screen -f 60 -a "$(pactl get-default-sink).monitor" -a "$(pactl get-default-source)" -c mp4 -o ~/Videos/Captures/capture_$(date +%Y-%m-%d_%H-%M-%S).mp4
