0033862d044fc0c6fe2b3f611f31bfcf51476829
Buck <kbuck@tuta.io>
2025-12-02 08:16:34 +0200

    Add files via upload

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,28 +12,30 @@ end
 
 set -g fish_color_valid_path normal
 
-set fish_cursor_default beam
-set fish_cursor_insert line
 set fish_cursor_replace_one underscore
 set fish_cursor_replace underscore
 set fish_cursor_external line
 set fish_cursor_visual block
+set fish_cursor_default beam
+set fish_cursor_insert line
 
-set -g fish_color_normal white
-set -g fish_color_command green
-set -g fish_color_param cyan
-set -g fish_color_comment red
 set -g fish_color_operator yellow
+set -g fish_color_command green
+set -g fish_color_normal white
+set -g fish_color_comment red
+set -g fish_color_param cyan
 
+set -g fish_color_history_current white
+set -g fish_color_search_match bryellow
+set -g fish_color_redirection blue
+set -g fish_color_selection white
 set -g fish_color_escape yellow
 set -g fish_color_quote magenta
-set -g fish_color_redirection blue
+set -g fish_color_match white
+set -g fish_color_host white
 set -g fish_color_end white
 set -g fish_color_error red
-set -g fish_color_match white
-set -g fish_color_selection white
-set -g fish_color_search_match bryellow
-set -g fish_color_history_current white
-set -g fish_color_host white
 
 alias ls="eza -G --icons"
+
+fish_add_path /home/buck/.spicetify
