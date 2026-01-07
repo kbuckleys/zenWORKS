84262395cd79d1e0c10012a50424caeee883fd3f
Buck <kbuck@tuta.io>
2026-01-07 11:18:40 +0200

    Add files via upload

--- .config/scripts/nvidia.sh
+++ .config/scripts/nvidia.sh
@@ -6,10 +6,10 @@
 METRICS=$(nvidia-smi --query-gpu=utilization.gpu,temperature.gpu,memory.used --format=csv,noheader,nounits)
 GPU_UTIL=$(echo "$METRICS" | cut -d',' -f1)
 GPU_TEMP=$(echo "$METRICS" | cut -d',' -f2)
-GPU_VRAM=$(echo "$METRICS" | cut -d',' -f3)
+GPU_VRAM=$(echo "$METRICS" | cut -d',' -f3 | awk '{printf "%.1f", $1/1024}')
 
 if [ "$GPU_TEMP" -ge 70 ]; then
-  echo "GPU $GPU_UTIL%  VRAM${GPU_VRAM} MB \033[38;2;224;174;164m${GPU_TEMP}°C\033[0m"
+  echo "GPU $GPU_UTIL%  VRAM ${GPU_VRAM} GB \033[38;2;224;174;164m${GPU_TEMP}°C\033[0m"
 else
-  echo "GPU $GPU_UTIL%  VRAM${GPU_VRAM} MB ${GPU_TEMP}°C"
+  echo "GPU $GPU_UTIL%  VRAM ${GPU_VRAM} GB ${GPU_TEMP}°C"
 fi
