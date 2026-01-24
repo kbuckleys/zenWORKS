0c41d07b2ddf604a5937437a1d9c57de049b00c1
Buck <kbuck@tuta.io>
2026-01-25 00:02:22 +0200

    Add files via upload

---
+++ scripts/hyprshot.sh
@@ -0,0 +1,13 @@
+# ┌─┐┌─┐┌┐┌┬ ┬┌─┐┬─┐┬┌─┌─┐
+# ┌─┘├┤ │││││││ │├┬┘├┴┐└─┐
+# └─┘└─┘┘└┘└┴┘└─┘┴└─┴ ┴└─┘
+# https://github.com/kbuckleys/
+
+#!/bin/bash
+
+dir=~/Pictures/Screenshots
+mkdir -p "$dir"
+monitor=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
+file="$dir/$(date +%Y%m%d-%H%M%S)-$monitor.png"
+grim -o "$monitor" "$file"
+notify-send -i "$file" "Screenshot" "Saved to $file"
