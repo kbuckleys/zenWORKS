a4b13e5e06b4b8e48b2117b5840ae6fe913f0a81
Buck <kbuck@tuta.io>
2026-02-26 18:00:17 +0200

    Add files via upload

--- .config/hypr/scripts/special.sh
+++ .config/hypr/scripts/special.sh
@@ -7,11 +7,14 @@
 #!/bin/bash
 
 toggle=0
+
 hyprctl keyword animation "workspacesOut, 1, 4, default, slidevertfade top"
-hyprctl keyword animation "workspacesIn, 1, 4, default, slidevertfade bottom"
+hyprctl keyword animation "workspacesIn, 1, 2, default, slidevertfade bottom"
+
 if [[ $(hyprctl activeworkspace -j | jq '.id') -eq 9 ]]; then toggle=1; fi
 hyprctl dispatch workspace 9
 [[ "$toggle" -eq 1 ]] || hyprctl dispatch togglespecialworkspace magic
 echo $toggle
+
 hyprctl keyword animation "workspacesOut, 1, 2, default, slidefade 15%"
 hyprctl keyword animation "workspacesIn, 1, 2, default, slidefade 15%"
