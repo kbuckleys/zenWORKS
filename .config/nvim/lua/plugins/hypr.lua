198c27cd688e811232dac17e852fa626b90828d1
Buck <kbuck@tuta.io>
2026-05-13 08:23:36 +0300

    Add files via upload

---
+++ .config/nvim/lua/plugins/hypr.lua
@@ -0,0 +1,27 @@
+-- ┌─┐┌─┐┌┐┌┬ ┬┌─┐┬─┐┬┌─┌─┐
+-- ┌─┘├┤ │││││││ │├┬┘├┴┐└─┐
+-- └─┘└─┘┘└┘└┴┘└─┘┴└─┴ ┴└─┘
+-- https://github.com/kbuckleys/
+
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              diagnostics = { globals = { "hl" } },
+              workspace = {
+                library = {
+                  "/usr/share/hypr/stubs",
+                  vim.fn.stdpath("config") .. "/lua",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
