f7838b2d181f80bba67547a66833a162b186c405
Buck <kbuck@tuta.io>
2026-05-14 13:46:27 +0300

    Add files via upload

---
+++ .config/hypr/rules.lua
@@ -0,0 +1,102 @@
+-- ┌─┐┌─┐┌┐┌┬ ┬┌─┐┬─┐┬┌─┌─┐
+-- ┌─┘├┤ │││││││ │├┬┘├┴┐└─┐
+-- └─┘└─┘┘└┘└┴┘└─┘┴└─┴ ┴└─┘
+-- https://github.com/kbuckleys/
+
+hl.window_rule({
+	match = { title = "System Update" },
+	float = true,
+	center = true,
+	size = { 1000, 1100 },
+})
+
+hl.window_rule({
+	match = { title = "sysmon" },
+	float = true,
+	center = true,
+	size = { 1000, 1100 },
+})
+
+hl.window_rule({
+	match = { class = "steam", title = "Steam Settings" },
+	float = true,
+})
+
+hl.window_rule({
+	match = { title = "Wiremix" },
+	float = true,
+	center = true,
+	size = { 650, 650 },
+})
+
+hl.window_rule({
+	match = { title = "PARUZ" },
+	float = true,
+	center = true,
+	size = { 900, 1000 },
+})
+
+hl.window_rule({
+	match = { title = "bandwhich" },
+	float = true,
+	size = { 850, 850 },
+})
+
+hl.window_rule({
+	match = { initial_title = "Friends List" },
+	float = true,
+})
+
+hl.window_rule({
+	match = { class = "swayimg" },
+	float = true,
+	center = true,
+})
+
+hl.window_rule({
+	match = { class = "mpv" },
+	float = true,
+	center = true,
+})
+
+-- HELIUM
+hl.window_rule({
+	match = { class = "xdg-desktop-portal-gtk", title = "Open Files" },
+	float = true,
+})
+
+hl.window_rule({
+	match = { class = "xdg-desktop-portal-gtk", title = "All Files" },
+	float = true,
+})
+
+hl.window_rule({
+	match = { class = "xdg-desktop-portal-gtk", title = "Save File" },
+	float = true,
+})
+
+hl.window_rule({
+	match = { class = "xdg-desktop-portal-gtk", title = "Open File" },
+	float = true,
+})
+
+hl.window_rule({
+	match = { class = "xdg-desktop-portal-gtk", title = "&Print…" },
+	float = true,
+})
+
+hl.window_rule({
+	match = { class = "xdg-desktop-portal-gtk", title = "Print" },
+	float = true,
+})
+
+-- BORDERS
+hl.window_rule({
+	match = { fullscreen = true },
+	border_color = "rgba(fab38799)",
+})
+
+hl.window_rule({
+	match = { float = true },
+	border_color = "rgba(b6e0a499)",
+})
