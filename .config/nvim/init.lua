8e02e263400e7ba2c8093a934ca8a17accb0ab02
Buck <kbuck@tuta.io>
2026-05-22 17:07:46 +0300

    Add files via upload

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,3 +7,10 @@ require("pack")
 require("options")
 require("colors")
 require("binds")
+
+-- Kill all child processes on exit
+vim.api.nvim_create_autocmd("VimLeave", {
+  callback = function()
+    vim.cmd("silent !kill $(pgrep -P $$)")
+  end,
+})
