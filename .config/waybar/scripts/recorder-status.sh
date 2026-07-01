25ed3cdd44c8b0b785ffd6c3279fe682e3fb0864
Buck <kbuck@tuta.io>
2026-07-02 00:00:53 +0300

    Add files via upload

---
+++ .config/waybar/scripts/recorder-status.sh
@@ -0,0 +1,12 @@
+# ┌─┐┌─┐┌┐┌┬ ┬┌─┐┬─┐┬┌─┌─┐
+# ┌─┘├┤ │││││││ │├┬┘├┴┐└─┐
+# └─┘└─┘┘└┘└┴┘└─┘┴└─┴ ┴└─┘
+# https://github.com/kbuckleys/
+
+#!/bin/sh
+
+if pgrep -x "wf-recorder" > /dev/null; then
+  echo '{"text":"","class":"recording"}'
+else
+  echo '{"text":""}'
+fi
