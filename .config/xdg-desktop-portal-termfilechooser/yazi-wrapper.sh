c42e0ff9b1a798123012fe0019c98ddf89efa292
Buck <kbuck@tuta.io>
2026-07-19 01:14:56 +0300

    Add files via upload

---
+++ .config/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env sh
+
+# ┌─┐┌─┐┌┐┌┬ ┬┌─┐┬─┐┬┌─┌─┐
+# ┌─┘├┤ │││││││ │├┬┘├┴┐└─┐
+# └─┘└─┘┘└┘└┴┘└─┘┴└─┴ ┴└─┘
+# https://github.com/kbuckleys/
+
+multiple="$1"
+directory="$2"
+save="$3"
+path="$4"
+out="$5"
+debug="$6"
+
+set -e
+
+if [ "$debug" = 1 ]; then
+    set -x
+fi
+
+cmd="yazi"
+termcmd="${TERMCMD:-kitty -1 --title 'termfilechooser'}"
+
+if [ "$save" = "1" ]; then
+    set -- --chooser-file="$out" "$path"
+elif [ "$directory" = "1" ]; then
+    set -- --chooser-file="$out" --cwd-file="$out"".1" "$path"
+elif [ "$multiple" = "1" ]; then
+    set -- --chooser-file="$out" "$path"
+else
+    set -- --chooser-file="$out" "$path"
+fi
+
+command="$termcmd $cmd"
+for arg in "$@"; do
+    escaped=$(printf "%s" "$arg" | sed 's/"/\\"/g')
+    command="$command \"$escaped\""
+done
+
+echo "TERMCMD='$TERMCMD'" >> /tmp/termfilechooser.log
+echo "termcmd='$termcmd'" >> /tmp/termfilechooser.log
+echo "command='$command'" >> /tmp/termfilechooser.log
+
+sh -c "$command"
+
+if [ "$directory" = "1" ]; then
+    if [ ! -s "$out" ] && [ -s "$out"".1" ]; then
+        cat "$out"".1" > "$out"
+        rm "$out"".1"
+    else
+        rm "$out"".1"
+    fi
+fi
