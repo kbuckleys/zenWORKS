4df7f55be22ea9d50c4f77e08cc76caa2f2850c5
Buck <kbuck@tuta.io>
2026-07-19 14:09:12 +0300

    Add files via upload

--- .config/hypr/lua/hyprshot.lua
+++ .config/hypr/lua/hyprshot.lua
@@ -9,12 +9,33 @@ local function screen()
 mkdir -p "]] .. dir .. [["
 monitor=$(hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
 file="]] .. dir .. [[/$(date +'%Y-%m-%d-%H%M%S')-$monitor.png"
-grim -o "$monitor" "$file" && wl-copy < "$file" && notify-send -i "$file" "Screenshot saved" "Saved to $file and copied to clipboard"
+grim -o "$monitor" "$file" && convert "$file" -background black -flatten "$file" && wl-copy < "$file" && notify-send -i "$file" "Screenshot saved" "Saved to $file and copied to clipboard"
+]]
+    hl.exec_cmd("bash -c '" .. script:gsub("'", "'\"'\"'") .. "'")
+end
+
+local function region()
+    local dir = os.getenv("HOME") .. "/Pictures/Screenshots"
+    local script = [[
+mkdir -p "]] .. dir .. [["
+file="]] .. dir .. [[/$(date +'%Y-%m-%d-%H%M%S')_region.png"
+grim -g "$(slurp -d)" "$file" && wl-copy < "$file" && notify-send -i "$file" "Screenshot saved" "Saved to $file and copied to clipboard"
+]]
+    hl.exec_cmd("bash -c '" .. script:gsub("'", "'\"'\"'") .. "'")
+end
+
+local function window()
+    local dir = os.getenv("HOME") .. "/Pictures/Screenshots"
+    local script = [[
+mkdir -p "]] .. dir .. [["
+file="]] .. dir .. [[/$(date +'%Y-%m-%d-%H%M%S')_window.png"
+geometry=$(hyprctl -j activewindow | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')
+grim -g "$geometry" "$file" && wl-copy < "$file" && notify-send -i "$file" "Screenshot saved" "Saved to $file and copied to clipboard"
 ]]
     hl.exec_cmd("bash -c '" .. script:gsub("'", "'\"'\"'") .. "'")
 end
 
 -- BINDS
-hl.bind("SUPER + SHIFT + PRINT", hl.dsp.exec_cmd("hyprshot -m region -o ~/Pictures/Screenshots/"))
-hl.bind("SUPER + PRINT", hl.dsp.exec_cmd("hyprshot -m window -o ~/Pictures/Screenshots/"))
+hl.bind("SUPER + SHIFT + PRINT", region)
+hl.bind("SUPER + PRINT", window)
 hl.bind("PRINT", screen)
